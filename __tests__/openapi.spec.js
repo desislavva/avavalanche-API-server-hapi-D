f453d845796c6be6168e3573376d701fe5b9e6fe
Desi <dessislv28th@gmail.com>
2021-10-19 23:04:59 +0300

    test

--- __tests__/openapi.spec.js
+++ __tests__/openapi.spec.js
@@ -1,8 +1,7 @@
 const jestOpenAPI = require('jest-openapi').default;
 const { default: axios } = require('axios');
 const path = require('path');
-require('dotenv').config();
-
+require('dotenv').config()
 
 jest.setTimeout(50000);
 
@@ -10,7 +9,7 @@ jestOpenAPI(path.join(__dirname, '../openapi/openapi.yml'));
 
 describe('GET /network', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(`http://localhost:4444/network`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/network`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -21,7 +20,7 @@ describe('GET /network', () => {
 describe('GET /address/hash/{hash}', () => {
     it('should satisfy OpenAPI spec', async () => {
         const hash = 'X-fuji1xpmx0ljrpvqexrvrj26fnggvr0ax9wm32gaxmx'
-        const res = await axios.get(`http://localhost:4444/address/hash/${hash}`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/address/hash/${hash}`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -32,7 +31,7 @@ describe('GET /address/hash/{hash}', () => {
 describe('GET /blocks/hash/{hash}', () => {
     it('should satisfy OpenAPI spec', async () => {
         const hash = '0x0bcd0c4e5635f21dd4352aa82692a5e29bcf2c5373da9427e5ab38bd4c7cfd33'
-        const res = await axios.get(`http://localhost:4444/blocks/hash/${hash}`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/blocks/hash/${hash}`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -43,7 +42,7 @@ describe('GET /blocks/hash/{hash}', () => {
 describe('GET /blocks/number/{blockNumber}', () => {
     it('should satisfy OpenAPI spec', async () => {
         const blockNumber = 1940150;
-        const res = await axios.get(`http://localhost:4444/blocks/number/${blockNumber}`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/blocks/number/${blockNumber}`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -54,7 +53,7 @@ describe('GET /blocks/number/{blockNumber}', () => {
 describe('GET /transactions/hash/{hash}', () => {
     it('should satisfy OpenAPI spec', async () => {
         const hash = '0x118e1747566adeaab6afede9de76ebeb5b10bb56ec510a099fb5a82221e9d0e7';
-        const res = await axios.get(`http://localhost:4444/transactions/hash/${hash}`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/transactions/hash/${hash}`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -67,7 +66,7 @@ describe('GET /transactions/{address}/{n}/{x}', () => {
         const address = 'X-fuji1xpmx0ljrpvqexrvrj26fnggvr0ax9wm32gaxmx';
         const n = 10;
         const x = 5;
-        const res = await axios.get(`http://localhost:4444/transactions/${address}/${n}/${x}`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/transactions/${address}/${n}/${x}`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -79,7 +78,7 @@ describe('GET /transactions/{n}/{x}', () => {
     it('should satisfy OpenAPI spec', async () => {
         const n = 10;
         const x = 5;
-        const res = await axios.get(`http://localhost:4444/transactions/${n}/${x}`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/transactions/${n}/${x}`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -89,7 +88,7 @@ describe('GET /transactions/{n}/{x}', () => {
 
 describe('GET /transactions/recentxchain', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(`http://localhost:4444/transactions/recentxchain`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/transactions/recentxchain`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
@@ -99,7 +98,7 @@ describe('GET /transactions/recentxchain', () => {
 
 describe('GET /transactions/recentpchain', () => {
     it('should satisfy OpenAPI spec', async () => {
-        const res = await axios.get(`http://localhost:4444/transactions/recentpchain`)
+        const res = await axios.get(`http://${process.env.SERVER_ADDRESS}/transactions/recentpchain`)
 
         expect(res.status).toEqual(200);
         expect(res).toSatisfyApiSpec();
